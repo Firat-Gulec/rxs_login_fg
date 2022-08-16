e1533382b2dd2f1d74c704613d22ea50b2b808ba
Fırat-Gulec <firat_gulec@hotmail.com>
2022-08-16 13:55:44 +0300

    first work

---
+++ lib/login/service/auth_manager.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:rxs_login_fg/login/service/cache_manager.dart';
+
+
+
+class AuthenticationManager extends CacheManager {
+  BuildContext context;
+  AuthenticationManager({
+    required this.context,
+  }) {
+    fetchUserLogin();
+  }
+  bool isLogin = false;
+
+  Future<void> fetchUserLogin() async {
+    final token = await getUserLogin();
+    if (token != null) {
+      isLogin = true;
+    }
+  }
+}
